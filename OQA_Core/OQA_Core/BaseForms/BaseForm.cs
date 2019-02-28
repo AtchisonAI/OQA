abac33bbf091a85afef9476ac3b2b7463e67eba5
E100812 <E100812>
2019-02-28 13:31:13 +0800

    开发环境项目重定义

---
+++ OQA_Core/OQA_Core/BaseForms/BaseForm.cs
@@ -0,0 +1,17 @@
+﻿using Syncfusion.Windows.Forms;
+
+namespace OQA_Core
+{
+    public partial class BaseForm : MetroForm
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseForm_Load(object sender, System.EventArgs e)
+        {
+            AuthorityControl.InitializeAuthority(this);
+        }
+    }
+}

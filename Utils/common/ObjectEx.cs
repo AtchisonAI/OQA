ed75907165259c04be71b719480efcb67b6a54c2
亮 陈 <coder.chen@hidmgroup.com>
2019-03-08 15:29:41 +0800

    增加获取系统时间方法

--- Utils/common/ObjectEx.cs
+++ Utils/common/ObjectEx.cs
@@ -86,5 +86,36 @@ namespace Utils
                 }
             }
         }
+
+        /// <summary>
+        /// 将数据库中变量名改为驼峰命名
+        /// 如 user_name 改为 UserName
+        /// </summary>
+        /// <param name="name">变量名</param>
+        /// <returns></returns>
+        public static string GenVarName(string name)
+        {
+            string first = name.Substring(0, 1);
+            name = name.Substring(1, name.Length - 1).ToLower();
+            name = first.ToUpper() + name;
+
+            int index = name.IndexOf("_");
+            while (index != -1)
+            {
+                if (name.Length >= index + 2)
+                {
+                    first = name.Substring(index + 1, 1);
+                    string start = name.Substring(0, index);
+                    string end = name.Substring(index + 2, name.Length - index - 2);
+                    name = start + first.ToUpper() + end;
+
+                    index = name.IndexOf("_");
+                }
+            }
+
+            name = name.Replace("_", "");
+
+            return name;
+        }
     }
 }

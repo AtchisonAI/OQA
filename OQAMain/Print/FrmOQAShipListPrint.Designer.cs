37bb0c9b85717a40387aace5628c2a5f02d011be
E100341 <lu.zhang@hidmgroup.com>
2019-03-11 09:44:54 +0800

    UPDATE

--- OQAMain/Print/FrmOQAShipListPrint.Designer.cs
+++ OQAMain/Print/FrmOQAShipListPrint.Designer.cs
@@ -28,24 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.pnlMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // BaseForm
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(646, 11);
+            this.btnClose.Text = "Ptint";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(432, 10);
+            this.btnCreate.Visible = false;
+            // 
+            // btnEdite
+            // 
+            this.btnEdite.Location = new System.Drawing.Point(541, 10);
+            this.btnEdite.Text = "Export";
+            this.btnEdite.Click += new System.EventHandler(this.btnEdite_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(3, 13);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 520);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "已生成的交接单";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dataGridView3);
+            this.panel1.Location = new System.Drawing.Point(0, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(227, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(0, 3);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 23;
+            this.dataGridView3.Size = new System.Drawing.Size(227, 150);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "交接单号：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(272, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(465, 170);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "发货批次信息";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(459, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.reportViewer2);
+            this.groupBox3.Location = new System.Drawing.Point(265, 188);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(470, 345);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "交接单打印预览";
+            // 
+            // reportViewer2
+            // 
+            this.reportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "transferForm.IOQA.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(3, 17);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.BearerToken = null;
+            this.reportViewer2.Size = new System.Drawing.Size(464, 325);
+            this.reportViewer2.TabIndex = 5;
+            // 
+            // FrmOQAShipListPrint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BorderColor = System.Drawing.Color.OliveDrab;
-            this.BorderThickness = 2;
-            this.ClientSize = new System.Drawing.Size(746, 548);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MinimumSize = new System.Drawing.Size(750, 580);
-            this.Name = "BaseForm";
-            this.Text = "BaseForm";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ClientSize = new System.Drawing.Size(750, 580);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "FrmOQAShipListPrint";
+            this.Load += new System.EventHandler(this.FrmOQAShipListPrint_Load);
+            this.Controls.SetChildIndex(this.pnlMenu, 0);
+            this.Controls.SetChildIndex(this.groupBox3, 0);
+            this.Controls.SetChildIndex(this.groupBox2, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.pnlMenu.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
     }
 }
--- OQAMain/Print/FrmOQAShipListPrint.cs
+++ OQAMain/Print/FrmOQAShipListPrint.cs
@@ -2,6 +2,14 @@
 using OQA_Core;
 using System;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+using System.Collections.Generic;
+using System.IO;
+using OQAMain.Print;
+using Zen.Barcode;
+using System.Drawing;
+using transferForm;
+using System.Data;
 
 namespace OQAMain
 {
@@ -152,7 +160,104 @@ namespace OQAMain
                 MessageBox.Show(ex.Message.ToString());
             }
         }
+        private string shipID;
+        public FrmOQAShipListPrint(string shipID)
+        {
+            InitializeComponent();
+            this.shipID = shipID;
+        }
+        private void FrmOQAShipListPrint_Load(object sender, EventArgs e)
+        {
 
+            textBox1.Text = shipID;
+            InitReportViewer("FrmOQAShipListPrint.rdlc");
+            //DataTable dtMaster = getShipMasterData(shipID);
+            //DataTable dtDetails = getOraclePKGSHPDAT(shipID);
 
+            //this.dataGridView1.DataSource = dtDetails;
+            //this.dataGridView1.AllowUserToAddRows = false;
+
+            //this.reportViewer2.LocalReport.DataSources.Clear();
+            //var dataSourceList = GenerateLabelDatasource(dtDetails);
+            //var dataSource = new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", dataSourceList);
+            //this.reportViewer2.LocalReport.DataSources.Add(dataSource);
+            //this.reportViewer2.LocalReport.SetParameters(GenerateLableParamters(dtMaster));
+            this.reportViewer2.RefreshReport();
+        }
+        public void InitReportViewer(string rptName)
+        {
+            reportViewer2.Reset();
+            reportViewer2.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Local;
+            reportViewer2.LocalReport.ReportPath = Path.Combine(rptName);
+            reportViewer2.RefreshReport();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+        //private DataTable getOraclePKGSHPDAT(string shipID)
+        //{
+        //    var sql = string.Format("SELECT A.LOT_ID,A.QTY,A.PART_ID,A.INSPECT_RESULT FROM PKGSHPDAT A WHERE SHIP_ID = '{0}'", shipID); ;
+        //    DataTable dataTable = OracleHelp.GetDataTable(sql);
+
+        //    return dataTable;
+        //}
+
+        //private DataTable getShipMasterData(string shipID)
+        //{
+        //    var sql = string.Format("SELECT A.PART_ID,A.QTY,A.SHIP_DATE FROM PKGSHPSTS A WHERE SHIP_ID = '{0}'", shipID); ;
+        //    DataTable dataTable = OracleHelp.GetDataTable(sql);
+
+        //    return dataTable;
+        //}
+
+        //打印表头内容
+        public List<ReportParameter> GenerateLableParamters(System.Data.DataTable dtMaster)
+        {
+            string PartID1 = dtMaster.Rows[0]["PART_ID"].ToString();
+            string Qtysys1 = dtMaster.Rows[0]["QTY"].ToString();
+            string Datesys1 = dtMaster.Rows[0]["SHIP_DATE"].ToString();
+            string codePackedDate = GenerateBarCodeByZen(shipID);
+
+            List<ReportParameter> lstParam = new List<ReportParameter>();
+            lstParam.Add(new ReportParameter("ParameterPartID", PartID1));
+            lstParam.Add(new ReportParameter("ParamWaferQty", Qtysys1));
+            lstParam.Add(new ReportParameter("ParamDate", Datesys1));
+
+           lstParam.Add(new ReportParameter("ParameterBarcode", codePackedDate));
+            return lstParam;
+        }
+       
+        //内容打印
+        public List<FrmOQAShipListPrintData> GenerateLabelDatasource(DataTable dtDetails)
+        {
+            List<FrmOQAShipListPrintData> result = new List<FrmOQAShipListPrintData>();
+            int index = 0;
+            foreach (DataRow row in dtDetails.Rows)
+            {
+                index++;
+                string lotID = row["LOT_ID"].ToString();
+                string qty = row["QTY"].ToString();
+                string inspResult = row["INSPECT_RESULT"].ToString();
+
+                result.Add(new FrmOQAShipListPrintData() { No = index.ToString(), LotID = lotID, LotQty = qty, InspectionRequest = inspResult });
+
+            }
+
+            return result;
+        }
+        //二维码转化
+        private string GenerateBarCodeByZen(string codeContent)
+        {
+            CodeQrBarcodeDraw qrcode = BarcodeDrawFactory.CodeQr;
+            Image img = qrcode.Draw(codeContent, qrcode.GetDefaultMetrics(40));
+            return img.ImageToBytes().ToBase64();
+
+        }
+
+        private void btnEdite_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }

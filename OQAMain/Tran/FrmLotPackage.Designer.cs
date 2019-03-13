1b6b1561fcc3d775d713075093760c977dbe4605
亮 陈 <coder.chen@hidmgroup.com>
2019-03-13 10:31:16 +0800

    lot package窗体设计

--- OQAMain/Tran/FrmLotPackage.Designer.cs
+++ OQAMain/Tran/FrmLotPackage.Designer.cs
@@ -28,24 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lotInfo_groupBox = new System.Windows.Forms.GroupBox();
+            this.lotId_textBox = new System.Windows.Forms.TextBox();
+            this.lotId_label = new System.Windows.Forms.Label();
+            this.package_groupBox = new System.Windows.Forms.GroupBox();
+            this.Attachment_imageUpload = new ImageUpload.ImageUpload();
+            this.attach_label = new System.Windows.Forms.Label();
+            this.PackageType_imageUpload = new ImageUpload.ImageUpload();
+            this.package_label = new System.Windows.Forms.Label();
+            this.ShipLabel_imageUpload = new ImageUpload.ImageUpload();
+            this.ship_label = new System.Windows.Forms.Label();
+            this.Fosb_imageUpload = new ImageUpload.ImageUpload();
+            this.fosb_label = new System.Windows.Forms.Label();
+            this.pnlMenu.SuspendLayout();
+            this.lotInfo_groupBox.SuspendLayout();
+            this.package_groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // BaseForm
+            // btnClose
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.btnClose.Location = new System.Drawing.Point(663, 10);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Size = new System.Drawing.Size(72, 32);
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(463, 10);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCreate.Size = new System.Drawing.Size(72, 32);
+            this.btnCreate.Visible = false;
+            // 
+            // btnEdite
+            // 
+            this.btnEdite.Location = new System.Drawing.Point(563, 10);
+            this.btnEdite.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdite.Size = new System.Drawing.Size(72, 32);
+            this.btnEdite.Click += new System.EventHandler(this.btnEdite_Click);
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            // 
+            // lblSucessMsg
+            // 
+            this.lblSucessMsg.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            // 
+            // lotInfo_groupBox
+            // 
+            this.lotInfo_groupBox.Controls.Add(this.lotId_textBox);
+            this.lotInfo_groupBox.Controls.Add(this.lotId_label);
+            this.lotInfo_groupBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lotInfo_groupBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lotInfo_groupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lotInfo_groupBox.Location = new System.Drawing.Point(0, 0);
+            this.lotInfo_groupBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.lotInfo_groupBox.Name = "lotInfo_groupBox";
+            this.lotInfo_groupBox.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.lotInfo_groupBox.Size = new System.Drawing.Size(750, 80);
+            this.lotInfo_groupBox.TabIndex = 1;
+            this.lotInfo_groupBox.TabStop = false;
+            this.lotInfo_groupBox.Text = "Lot Information";
+            // 
+            // lotId_textBox
+            // 
+            this.lotId_textBox.Location = new System.Drawing.Point(85, 36);
+            this.lotId_textBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.lotId_textBox.Name = "lotId_textBox";
+            this.lotId_textBox.Size = new System.Drawing.Size(190, 21);
+            this.lotId_textBox.TabIndex = 1;
+            this.lotId_textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotId_textBox_KeyPress);
+            // 
+            // lotId_label
+            // 
+            this.lotId_label.AutoSize = true;
+            this.lotId_label.Location = new System.Drawing.Point(30, 38);
+            this.lotId_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lotId_label.Name = "lotId_label";
+            this.lotId_label.Size = new System.Drawing.Size(42, 15);
+            this.lotId_label.TabIndex = 0;
+            this.lotId_label.Text = "Lot ID:";
+            // 
+            // package_groupBox
+            // 
+            this.package_groupBox.Controls.Add(this.Attachment_imageUpload);
+            this.package_groupBox.Controls.Add(this.attach_label);
+            this.package_groupBox.Controls.Add(this.PackageType_imageUpload);
+            this.package_groupBox.Controls.Add(this.package_label);
+            this.package_groupBox.Controls.Add(this.ShipLabel_imageUpload);
+            this.package_groupBox.Controls.Add(this.ship_label);
+            this.package_groupBox.Controls.Add(this.Fosb_imageUpload);
+            this.package_groupBox.Controls.Add(this.fosb_label);
+            this.package_groupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.package_groupBox.Location = new System.Drawing.Point(0, 80);
+            this.package_groupBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.package_groupBox.Name = "package_groupBox";
+            this.package_groupBox.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.package_groupBox.Size = new System.Drawing.Size(750, 450);
+            this.package_groupBox.TabIndex = 2;
+            this.package_groupBox.TabStop = false;
+            this.package_groupBox.Text = "Wafer Package";
+            // 
+            // Attachment_imageUpload
+            // 
+            this.Attachment_imageUpload.Location = new System.Drawing.Point(256, 184);
+            this.Attachment_imageUpload.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Attachment_imageUpload.Name = "Attachment_imageUpload";
+            this.Attachment_imageUpload.PicStream = null;
+            this.Attachment_imageUpload.Size = new System.Drawing.Size(350, 42);
+            this.Attachment_imageUpload.TabIndex = 1;
+            // 
+            // attach_label
+            // 
+            this.attach_label.AutoSize = true;
+            this.attach_label.Location = new System.Drawing.Point(30, 197);
+            this.attach_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.attach_label.Name = "attach_label";
+            this.attach_label.Size = new System.Drawing.Size(68, 15);
+            this.attach_label.TabIndex = 0;
+            this.attach_label.Text = "Attachment";
+            // 
+            // PackageType_imageUpload
+            // 
+            this.PackageType_imageUpload.Location = new System.Drawing.Point(256, 135);
+            this.PackageType_imageUpload.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.PackageType_imageUpload.Name = "PackageType_imageUpload";
+            this.PackageType_imageUpload.PicStream = null;
+            this.PackageType_imageUpload.Size = new System.Drawing.Size(350, 42);
+            this.PackageType_imageUpload.TabIndex = 1;
+            // 
+            // package_label
+            // 
+            this.package_label.AutoSize = true;
+            this.package_label.Location = new System.Drawing.Point(30, 146);
+            this.package_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.package_label.Name = "package_label";
+            this.package_label.Size = new System.Drawing.Size(80, 15);
+            this.package_label.TabIndex = 0;
+            this.package_label.Text = "Package type";
+            // 
+            // ShipLabel_imageUpload
+            // 
+            this.ShipLabel_imageUpload.Location = new System.Drawing.Point(256, 84);
+            this.ShipLabel_imageUpload.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.ShipLabel_imageUpload.Name = "ShipLabel_imageUpload";
+            this.ShipLabel_imageUpload.PicStream = null;
+            this.ShipLabel_imageUpload.Size = new System.Drawing.Size(350, 42);
+            this.ShipLabel_imageUpload.TabIndex = 1;
+            // 
+            // ship_label
+            // 
+            this.ship_label.AutoSize = true;
+            this.ship_label.Location = new System.Drawing.Point(30, 95);
+            this.ship_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ship_label.Name = "ship_label";
+            this.ship_label.Size = new System.Drawing.Size(136, 15);
+            this.ship_label.TabIndex = 0;
+            this.ship_label.Text = "Shipping label attached";
+            // 
+            // Fosb_imageUpload
+            // 
+            this.Fosb_imageUpload.Location = new System.Drawing.Point(256, 32);
+            this.Fosb_imageUpload.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Fosb_imageUpload.Name = "Fosb_imageUpload";
+            this.Fosb_imageUpload.PicStream = null;
+            this.Fosb_imageUpload.Size = new System.Drawing.Size(350, 42);
+            this.Fosb_imageUpload.TabIndex = 1;
+            // 
+            // fosb_label
+            // 
+            this.fosb_label.AutoSize = true;
+            this.fosb_label.Location = new System.Drawing.Point(30, 44);
+            this.fosb_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fosb_label.Name = "fosb_label";
+            this.fosb_label.Size = new System.Drawing.Size(209, 15);
+            this.fosb_label.TabIndex = 0;
+            this.fosb_label.Text = "No chipping,crack and cross in FOSB";
+            // 
+            // FrmLotPackage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BorderColor = System.Drawing.Color.OliveDrab;
-            this.BorderThickness = 2;
-            this.ClientSize = new System.Drawing.Size(746, 548);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MinimumSize = new System.Drawing.Size(750, 580);
-            this.Name = "BaseForm";
-            this.Text = "BaseForm";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ClientSize = new System.Drawing.Size(750, 580);
+            this.Controls.Add(this.package_groupBox);
+            this.Controls.Add(this.lotInfo_groupBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmLotPackage";
+            this.Text = "Lot Package";
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            this.Controls.SetChildIndex(this.pnlMenu, 0);
+            this.Controls.SetChildIndex(this.lotInfo_groupBox, 0);
+            this.Controls.SetChildIndex(this.package_groupBox, 0);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            this.lotInfo_groupBox.ResumeLayout(false);
+            this.lotInfo_groupBox.PerformLayout();
+            this.package_groupBox.ResumeLayout(false);
+            this.package_groupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox lotInfo_groupBox;
+        private System.Windows.Forms.TextBox lotId_textBox;
+        private System.Windows.Forms.Label lotId_label;
+        private System.Windows.Forms.GroupBox package_groupBox;
+        private ImageUpload.ImageUpload Fosb_imageUpload;
+        private System.Windows.Forms.Label fosb_label;
+        private ImageUpload.ImageUpload Attachment_imageUpload;
+        private System.Windows.Forms.Label attach_label;
+        private ImageUpload.ImageUpload PackageType_imageUpload;
+        private System.Windows.Forms.Label package_label;
+        private ImageUpload.ImageUpload ShipLabel_imageUpload;
+        private System.Windows.Forms.Label ship_label;
     }
 }
--- OQAMain/Tran/FrmMarcoInput.Designer.cs
+++ OQAMain/Tran/FrmMarcoInput.Designer.cs
@@ -55,21 +55,25 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(657, 8);
+            this.btnClose.Location = new System.Drawing.Point(797, 10);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(469, 8);
+            this.btnCreate.Location = new System.Drawing.Point(578, 10);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnEdite
             // 
-            this.btnEdite.Location = new System.Drawing.Point(563, 8);
+            this.btnEdite.Location = new System.Drawing.Point(688, 10);
+            this.btnEdite.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             // 
             // pnlMenu
             // 
-            this.pnlMenu.Location = new System.Drawing.Point(0, 736);
-            this.pnlMenu.Size = new System.Drawing.Size(777, 40);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 920);
+            this.pnlMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pnlMenu.Size = new System.Drawing.Size(906, 50);
             // 
             // groupBox2
             // 
@@ -77,9 +81,11 @@
             this.groupBox2.Controls.Add(this.label15);
             this.groupBox2.Controls.Add(this.lotTextBox);
             this.groupBox2.Controls.Add(this.label16);
-            this.groupBox2.Location = new System.Drawing.Point(24, 45);
+            this.groupBox2.Location = new System.Drawing.Point(28, 56);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(721, 94);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(841, 118);
             this.groupBox2.TabIndex = 39;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Macro inspection simple MAP";
@@ -88,9 +94,10 @@
             // 
             this.slotComboBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.slotComboBox.FormattingEnabled = true;
-            this.slotComboBox.Location = new System.Drawing.Point(411, 56);
+            this.slotComboBox.Location = new System.Drawing.Point(479, 70);
+            this.slotComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.slotComboBox.Name = "slotComboBox";
-            this.slotComboBox.Size = new System.Drawing.Size(121, 22);
+            this.slotComboBox.Size = new System.Drawing.Size(140, 22);
             this.slotComboBox.TabIndex = 4;
             this.slotComboBox.SelectedIndexChanged += new System.EventHandler(this.slotComboBox_SelectedIndexChanged);
             // 
@@ -98,7 +105,7 @@
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label15.Location = new System.Drawing.Point(81, 57);
+            this.label15.Location = new System.Drawing.Point(94, 71);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(49, 14);
             this.label15.TabIndex = 1;
@@ -107,17 +114,18 @@
             // lotTextBox
             // 
             this.lotTextBox.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lotTextBox.Location = new System.Drawing.Point(157, 56);
+            this.lotTextBox.Location = new System.Drawing.Point(183, 70);
+            this.lotTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lotTextBox.Multiline = true;
             this.lotTextBox.Name = "lotTextBox";
-            this.lotTextBox.Size = new System.Drawing.Size(100, 21);
+            this.lotTextBox.Size = new System.Drawing.Size(116, 25);
             this.lotTextBox.TabIndex = 3;
             // 
             // label16
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label16.Location = new System.Drawing.Point(348, 56);
+            this.label16.Location = new System.Drawing.Point(406, 70);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(35, 14);
             this.label16.TabIndex = 2;
@@ -132,36 +140,41 @@
             this.groupBox3.Controls.Add(this.decRichTextBox);
             this.groupBox3.Controls.Add(this.label5);
             this.groupBox3.Controls.Add(this.label9);
-            this.groupBox3.Location = new System.Drawing.Point(24, 538);
+            this.groupBox3.Location = new System.Drawing.Point(28, 672);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(721, 163);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox3.Size = new System.Drawing.Size(841, 204);
             this.groupBox3.TabIndex = 38;
             this.groupBox3.TabStop = false;
             // 
             // imageTextBox
             // 
             this.imageTextBox.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.imageTextBox.Location = new System.Drawing.Point(142, 23);
+            this.imageTextBox.Location = new System.Drawing.Point(166, 29);
+            this.imageTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.imageTextBox.Multiline = true;
             this.imageTextBox.Name = "imageTextBox";
-            this.imageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.imageTextBox.Size = new System.Drawing.Size(116, 24);
             this.imageTextBox.TabIndex = 12;
             // 
             // cmtRichTextBox
             // 
             this.cmtRichTextBox.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.cmtRichTextBox.Location = new System.Drawing.Point(142, 115);
+            this.cmtRichTextBox.Location = new System.Drawing.Point(166, 144);
+            this.cmtRichTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmtRichTextBox.Name = "cmtRichTextBox";
-            this.cmtRichTextBox.Size = new System.Drawing.Size(569, 29);
+            this.cmtRichTextBox.Size = new System.Drawing.Size(663, 35);
             this.cmtRichTextBox.TabIndex = 16;
             this.cmtRichTextBox.Text = "";
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Aharoni", 5.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button1.Location = new System.Drawing.Point(248, 20);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(289, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.Size = new System.Drawing.Size(27, 29);
             this.button1.TabIndex = 17;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
@@ -171,7 +184,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label8.Location = new System.Drawing.Point(35, 71);
+            this.label8.Location = new System.Drawing.Point(41, 89);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(84, 14);
             this.label8.TabIndex = 8;
@@ -180,9 +193,10 @@
             // decRichTextBox
             // 
             this.decRichTextBox.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.decRichTextBox.Location = new System.Drawing.Point(142, 62);
+            this.decRichTextBox.Location = new System.Drawing.Point(166, 78);
+            this.decRichTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.decRichTextBox.Name = "decRichTextBox";
-            this.decRichTextBox.Size = new System.Drawing.Size(569, 29);
+            this.decRichTextBox.Size = new System.Drawing.Size(663, 35);
             this.decRichTextBox.TabIndex = 15;
             this.decRichTextBox.Text = "";
             // 
@@ -190,7 +204,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label5.Location = new System.Drawing.Point(35, 25);
+            this.label5.Location = new System.Drawing.Point(41, 31);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(91, 14);
             this.label5.TabIndex = 5;
@@ -200,7 +214,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label9.Location = new System.Drawing.Point(35, 124);
+            this.label9.Location = new System.Drawing.Point(41, 155);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(56, 14);
             this.label9.TabIndex = 9;
@@ -214,9 +228,11 @@
             this.groupBox1.Controls.Add(this.waferSurF);
             this.groupBox1.Controls.Add(this.label14);
             this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.groupBox1.Location = new System.Drawing.Point(24, 134);
+            this.groupBox1.Location = new System.Drawing.Point(28, 168);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(721, 398);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(841, 498);
             this.groupBox1.TabIndex = 37;
             this.groupBox1.TabStop = false;
             // 
@@ -224,7 +240,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label13.Location = new System.Drawing.Point(96, 356);
+            this.label13.Location = new System.Drawing.Point(112, 445);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(301, 14);
             this.label13.TabIndex = 23;
@@ -233,9 +249,10 @@
             // frontButton
             // 
             this.frontButton.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold);
-            this.frontButton.Location = new System.Drawing.Point(260, 289);
+            this.frontButton.Location = new System.Drawing.Point(303, 361);
+            this.frontButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.frontButton.Name = "frontButton";
-            this.frontButton.Size = new System.Drawing.Size(123, 23);
+            this.frontButton.Size = new System.Drawing.Size(143, 29);
             this.frontButton.TabIndex = 31;
             this.frontButton.Text = "Front side";
             this.frontButton.UseVisualStyleBackColor = true;
@@ -244,7 +261,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("宋体", 10.5F);
-            this.label12.Location = new System.Drawing.Point(96, 329);
+            this.label12.Location = new System.Drawing.Point(112, 411);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(490, 14);
             this.label12.TabIndex = 22;
@@ -252,17 +269,17 @@
             // 
             // waferSurF
             // 
-            this.waferSurF.Location = new System.Drawing.Point(216, 70);
-            this.waferSurF.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.waferSurF.Location = new System.Drawing.Point(252, 88);
+            this.waferSurF.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.waferSurF.Name = "waferSurF";
-            this.waferSurF.Size = new System.Drawing.Size(210, 210);
+            this.waferSurF.Size = new System.Drawing.Size(245, 262);
             this.waferSurF.TabIndex = 27;
             // 
             // label14
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label14.Location = new System.Drawing.Point(151, 33);
+            this.label14.Location = new System.Drawing.Point(176, 41);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(439, 20);
             this.label14.TabIndex = 26;
@@ -270,12 +287,13 @@
             // 
             // FrmMarcoInput
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(777, 776);
+            this.ClientSize = new System.Drawing.Size(906, 970);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
             this.Name = "FrmMarcoInput";
             this.Load += new System.EventHandler(this.FrmMarcoInput_Load);
             this.Controls.SetChildIndex(this.pnlMenu, 0);

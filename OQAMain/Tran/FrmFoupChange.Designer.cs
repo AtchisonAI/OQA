1bba455913e997033c7bdf7a6f2171bd2111e414
E101125 <E101125@HAPC-E101125>
2019-03-21 15:58:44 +0800

    update

--- OQAMain/Tran/FrmFoupChange.Designer.cs
+++ OQAMain/Tran/FrmFoupChange.Designer.cs
@@ -69,17 +69,18 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(652, 6);
+            this.btnClose.Location = new System.Drawing.Point(646, 6);
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(558, 5);
+            this.btnCreate.Location = new System.Drawing.Point(552, 5);
             this.btnCreate.Text = "Submit";
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnEdite
             // 
-            this.btnEdite.Location = new System.Drawing.Point(558, 6);
+            this.btnEdite.Location = new System.Drawing.Point(552, 6);
             this.btnEdite.Visible = false;
             this.btnEdite.Click += new System.EventHandler(this.btnEdite_Click);
             // 
@@ -349,12 +350,13 @@
             // btnPrint
             // 
             this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPrint.Location = new System.Drawing.Point(464, 6);
+            this.btnPrint.Location = new System.Drawing.Point(244, 6);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(88, 26);
             this.btnPrint.TabIndex = 6;
             this.btnPrint.Text = "Print";
             this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Visible = false;
             this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // FrmFoupChange

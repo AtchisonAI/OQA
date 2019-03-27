7a65f7869bd2ae16996ba56f3260999d022b1cb8
E100341 <lu.zhang@hidmgroup.com>
2019-03-27 11:47:08 +0800

    1

--- OQAMain/Print/FrmOQAShipListPrint.cs
+++ OQAMain/Print/FrmOQAShipListPrint.cs
@@ -12,6 +12,7 @@ using transferForm;
 using System.Data;
 using WCFModels.Message;
 using WCFModels.OQA;
+using System.Linq;
 
 namespace OQAMain
 {
@@ -295,11 +296,11 @@ namespace OQAMain
                 //ComFunc.(LotIDList, true);
                 //      txtCount.Text = out_data.model.PKGSHPDAT_list.Count.ToString();
 
-
-                for (int i = 0; i < out_data.model.SHIPIDLIST_list.Count; i++)
+                List< PKGSHPSTS >SortByTime= out_data.model.SHIPIDLIST_list.OrderByDescending(o => o.CreateTime).ToList();
+                for (int i = 0; i < SortByTime.Count; i++)
                 {
                     ListViewItem list_item = new ListViewItem();
-                    PKGSHPSTS list = out_data.model.SHIPIDLIST_list[i];
+                    PKGSHPSTS list = SortByTime[i];
                     list_item.Text = list.ShipId;
                     CheckShipID.Items.Add(list_item.Text);
                 }
